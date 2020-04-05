fd85ec60d1a3a8c69f0d21195f07f95dae307b0b
AlexeyErmakov <lexlaeda30@gmail.com>
2020-04-05 21:56:20 +0300

    add new"

--- src/main/java/com/email/shipment/repository/PersonRepository.java
+++ src/main/java/com/email/shipment/repository/PersonRepository.java
@@ -7,20 +7,3 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PersonRepository extends MongoRepository<Person,Long> {
 }
- class HumanReadableTime {
-     public static void main(String[] args) {
-         System.out.println(makeReadable(142630));
-     }
-     public static String makeReadable(int seconds) {
-         int h = seconds/3600;
-         System.out.println(h);
-         int m = (seconds- h*3600)/60;
-         System.out.println(m);
-         int s = seconds - h * 3600 - m * 60;
-         System.out.println(s);
-         return format(h) + ":" + format(m) + ":" + format(s);
-     }
-     public static String format(int x){
-        return x >= 10 ? String.valueOf(x) : "0" + x;
-     }
-}
